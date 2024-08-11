f899983dd3f212605f2e082fa135056ce5335948
Raghunath Kunigiri <54011917+Raghunath9493@users.noreply.github.com>
2024-08-10 22:59:01 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# American Nursing College
+
+This is the official website of American Nursing College
+=======
+# American-Nursing-College-
+>>>>>>> origin/main
---
+++ js/main.js
@@ -0,0 +1,7 @@
+// // C:\Users\kunig\OneDrive - Saint Louis University\Documents\ASN\American-Nursing-College-\js\main.js
+// VanillaTilt.init(document.querySelectorAll(".card"), {
+//     max: 15,
+//     speed: 400,
+//     glare: true,
+//     "max-glare": .5
+// });
